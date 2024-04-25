ff361568d53a292d0fa2a1f0fd81aae7057edcb9
eydriyan <115596606+eydriyan@users.noreply.github.com>
2024-04-25 12:10:24 +0800

    minor update
    minor update

--- TicketingSystemBackend/src/main/java/com/group2/TicketingSystemBackend/controller/TechnicianController.java
+++ TicketingSystemBackend/src/main/java/com/group2/TicketingSystemBackend/controller/TechnicianController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("api/technicians")
+@RequestMapping("api/technician")
 public class TechnicianController {
     @Autowired
     private TechnicianService technicianService;
